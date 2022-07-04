50e791095fa2ce2edee223d46ba6b307f49205e3
al8n <scygliu1@gmail.com>
2022-07-05 00:02:05 +0800

    bump up versions

--- src/unix.rs
+++ src/unix.rs
@@ -51,36 +51,8 @@ pub fn lock_error() -> Error {
     Error::from_raw_os_error(rustix::io::Errno::WOULDBLOCK.raw_os_error())
 }
 
-#[cfg(any(target_os = "macos", target_os = "ios"))]
 pub fn statvfs(path: impl AsRef<Path>) -> Result<FsStats> {
-    use std::ffi::CString;
-    use std::io::ErrorKind;
-    use std::mem;
-    use std::os::unix::ffi::OsStrExt;
-
-    let cstr = match CString::new(path.as_ref().as_os_str().as_bytes()) {
-        Ok(cstr) => cstr,
-        Err(..) => return Err(Error::new(ErrorKind::InvalidInput, "path contained a null")),
-    };
-    unsafe {
-        let mut stat: libc::statvfs = mem::zeroed();
-        // danburkert/fs2-rs#1: cast is necessary for platforms where c_char != u8.
-        if libc::statvfs(cstr.as_ptr() as *const _, &mut stat) != 0 {
-            Err(Error::last_os_error())
-        } else {
-            Ok(FsStats {
-                free_space: stat.f_frsize as u64 * stat.f_bfree as u64,
-                available_space: stat.f_frsize as u64 * stat.f_bavail as u64,
-                total_space: stat.f_frsize as u64 * stat.f_blocks as u64,
-                allocation_granularity: stat.f_frsize as u64,
-            })
-        }
-    }
-}
-
-#[cfg(not(any(target_os = "macos", target_os = "ios")))]
-pub fn statvfs(path: impl AsRef<Path>) -> Result<FsStats> {
-    match rustix::fs::statfs(path.as_ref()) {
+    match rustix::fs::statvfs(path.as_ref()) {
         Ok(stat) => Ok(FsStats {
             free_space: stat.f_frsize as u64 * stat.f_bfree as u64,
             available_space: stat.f_frsize as u64 * stat.f_bavail as u64,
