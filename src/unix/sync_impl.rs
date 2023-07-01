820a061b1a677e8dea07fc13c13cbc69e10dfcf5
Jakub Onderka <ahoj@jakubonderka.cz>
2023-07-01 10:59:44 +0200

    Update to rustix 0.38

--- src/unix/sync_impl.rs
+++ src/unix/sync_impl.rs
@@ -26,7 +26,7 @@ pub fn allocate(file: &File, len: u64) -> std::io::Result<()> {
     };
     unsafe {
         let borrowed_fd = BorrowedFd::borrow_raw(file.as_raw_fd());
-        match fallocate(borrowed_fd, FallocateFlags::from_bits_unchecked(0), 0, len) {
+        match fallocate(borrowed_fd, FallocateFlags::empty(), 0, len) {
             Ok(_) => Ok(()),
             Err(e) => Err(std::io::Error::from_raw_os_error(e.raw_os_error())),
         }
